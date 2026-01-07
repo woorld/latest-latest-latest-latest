ce0be90d0975816241676097e5b39088d623d004
わーるど <17719812+woorld@users.noreply.github.com>
2026-01-07 21:03:44 +0900

    イベント登録が無制限に行われないよう修正 (#2)

--- content.js
+++ content.js
@@ -1,8 +1,19 @@
+let isEventRegistered = false;
+
 const bodyObserver = new MutationObserver(() => {
+  if (!location.href.startsWith('https://x.com/home')) {
+    isEventRegistered = false;
+    return;
+  }
+
+  if (isEventRegistered) {
+    return;
+  }
+
   const homeButton = document.querySelector('a[href="/home"].css-175oi2r.r-6koalj.r-eqz5dr.r-16y2uox.r-1habvwh.r-cnw61z.r-13qz1uu.r-1ny4l3l.r-1loqt21')
   const followingTab = document.querySelector('div[aria-haspopup="menu"].css-175oi2r.r-1awozwy.r-6koalj.r-eqz5dr.r-16y2uox.r-1h3ijdo.r-1777fci.r-s8bhmr.r-3pj75a.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l');
 
-  if (!location.href.startsWith('https://x.com/home') || homeButton == null || followingTab == null) {
+  if (homeButton == null || followingTab == null) {
     return;
   }
 
@@ -14,6 +25,7 @@ const bodyObserver = new MutationObserver(() => {
     e.stopImmediatePropagation();
     homeButton.click();
   });
+  isEventRegistered = true;
 });
 
 bodyObserver.observe(document.body, {
