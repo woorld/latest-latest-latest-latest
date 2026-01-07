84c84102a03d659dd4ce2364cdae1c5e3b753b3d
woorld <17719812+woorld@users.noreply.github.com>
2026-01-07 19:58:08 +0900

    メイン処理をcontent.jsとして追加

---
+++ content.js
@@ -0,0 +1,22 @@
+const bodyObserver = new MutationObserver(() => {
+  const homeButton = document.querySelector('a[href="/home"].css-175oi2r.r-6koalj.r-eqz5dr.r-16y2uox.r-1habvwh.r-cnw61z.r-13qz1uu.r-1ny4l3l.r-1loqt21')
+  const followingTab = document.querySelector('div[aria-haspopup="menu"].css-175oi2r.r-1awozwy.r-6koalj.r-eqz5dr.r-16y2uox.r-1h3ijdo.r-1777fci.r-s8bhmr.r-3pj75a.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l');
+
+  if (!location.href.startsWith('https://x.com/home') || homeButton == null || followingTab == null) {
+    return;
+  }
+
+  followingTab.addEventListener('click', e => {
+    const isChevronClick = ['svg', 'g', 'path'].includes(e.target.tagName);
+    if (followingTab.getAttribute('aria-selected') === 'false' || isChevronClick) {
+      return;
+    }
+    e.stopImmediatePropagation();
+    homeButton.click();
+  });
+});
+
+bodyObserver.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
