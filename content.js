444c331f36dfd1529cf46baa50887623cd721eac
わーるど <17719812+woorld@users.noreply.github.com>
2026-01-16 20:26:06 +0900

    Xのクラス変更に対応 (#5)

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ const bodyObserver = new MutationObserver(() => {
     return;
   }
 
-  const homeButton = document.querySelector('a[href="/home"].css-175oi2r.r-6koalj.r-eqz5dr.r-16y2uox.r-1habvwh.r-cnw61z.r-13qz1uu.r-1ny4l3l.r-1loqt21')
+  const homeButton = document.querySelector('a[href="/home"].css-175oi2r.r-6koalj.r-eqz5dr.r-16y2uox.r-cnw61z.r-13qz1uu.r-1ny4l3l.r-1loqt21');
   const followingTab = document.querySelector('div[aria-haspopup="menu"].css-175oi2r.r-1awozwy.r-6koalj.r-eqz5dr.r-16y2uox.r-1h3ijdo.r-1777fci.r-s8bhmr.r-3pj75a.r-1loqt21.r-o7ynqc.r-6416eg.r-1ny4l3l');
 
   if (homeButton == null || followingTab == null) {
